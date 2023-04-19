1150d7ecfceb67366bbfe75da2a86f1ca1b1b8f0
khangneko <khangt893@gmail.com>
2023-04-19 18:14:51 +0700

    Can upload image properly now

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,26 @@
 <template>
   <TopBar></TopBar>
-  <el-main style="display: flex; justify-content: center; flex-direction: column; align-items: center">
+  <el-main
+    style="
+      display: flex;
+      justify-content: center;
+      flex-direction: column;
+      align-items: center;
+    "
+  >
     <router-view
-        @updateBasic="updateBasic"
-        @updateLocation="updateLocation"
-        @updateRoom="updateRoom"
-        @updateUtility="updateUtility"
-        @updateRule="updateRule"
-        @updateInformation="updateInformation"
-        @updatePrice="updatePrice"
+      @updateBasic="updateBasic"
+      @updateLocation="updateLocation"
+      @updateRoom="updateRoom"
+      @updateUtility="updateUtility"
+      @updateRule="updateRule"
+      @updateInformation="updateInformation"
+      @updatePrice="updatePrice"
+      @updateImage="updateImage"
     />
   </el-main>
   <p></p>
   <el-footer style="font-size: small">
-    <p>Bài tập cho môn 2122I_INT3139, viết bởi Vinh.</p>
     <p>© 2021. Sản phẩm tuân theo MIT License.</p>
   </el-footer>
 </template>
@@ -23,8 +30,17 @@ import TopBar from "@/components/TopBar";
 import axios from "axios";
 
 export default {
-  name: 'App',
-  emits: ['updateBasic', 'updateLocation', 'updateRoom', 'updateUtility', 'updateRule', 'updateInformation', 'updatePrice'],
+  name: "App",
+  emits: [
+    "updateBasic",
+    "updateLocation",
+    "updateRoom",
+    "updateUtility",
+    "updateRule",
+    "updateInformation",
+    "updatePrice",
+    "updateImage",
+  ],
   components: {
     TopBar,
   },
@@ -32,65 +48,40 @@ export default {
   data() {
     return {
       full_info: {
-        "name": "",
-        "details": "",
-        "image": "https://cdn.luxstay.com/rooms/13737/large/room_13737_31_1566960034.jpg",
-        "address": "",
-        "place_type": "",
-        "schedule_price_attributes": {
-          "normal_day_price": 13.36,
-          "weekend_price": 17.81,
-          "cleaning_price": 4.01,
+        name: "",
+        details: "",
+        image: "",
+        address: "",
+        place_type: "",
+        schedule_price_attributes: {
+          normal_day_price: 13.36,
+          weekend_price: 17.81,
+          cleaning_price: 4.01,
         },
-        "policy_attributes": {
-          "currency": "vnd",
-          "max_num_of_people": 1,
-          "cancel_policy": "normal"
+        policy_attributes: {
+          currency: "vnd",
+          max_num_of_people: 1,
+          cancel_policy: "normal",
         },
-        "overviews_attributes": [
-          {
-            "image": "https://cdn.luxstay.com/users/32999/c1gK94DTkZ4l3NpJR2TwskZR.jpg",
-          },
-          {
-            "image": "https://cdn.luxstay.com/rooms/32866/large/Untitled_HDR2%2050.jpg",
-          },
-          {
-            "image": "https://cdn.luxstay.com/rooms/33385/large/LIM%20GREEN%20(16).jpg",
-          },
-          {
-            "image": "https://cdn.luxstay.com/rooms/25495/large/room_25495_52_1558713239.jpg",
-          },
-          {
-            "image": "https://cdn.luxstay.com/users/230391/WMPuXP_zH_qJ8vfz8nrpQSW7.jpg",
-          },
-          {
-            "image": "https://cdn.luxstay.com/rooms/13737/large/room_13737_31_1566960034.jpg",
-          },
-          {
-            "image": "https://cdn.luxstay.com/rooms/23569/large/339a03a1810e7950201f.jpg",
-          },
-          {
-            "image": "https://cdn.luxstay.com/users/32999/c1gK94DTkZ4l3NpJR2TwskZR.jpg",
-          }
-        ],
-        "rule_attributes": {
-          "special_rules": "no special rules",
-          "smoking": "allowed",
-          "pet": "allowed",
-          "cooking": "unallowed",
-          "party": "charge"
+        overviews_attributes: [],
+        rule_attributes: {
+          special_rules: "no special rules",
+          smoking: "allowed",
+          pet: "allowed",
+          cooking: "unallowed",
+          party: "charge",
         },
-        "room_attributes": {
-          "square": 25,
-          "num_of_bedroom": 1,
-          "num_of_bed": 1,
-          "num_of_bathroom": 1,
-          "num_of_kitchen": 1,
+        room_attributes: {
+          square: 25,
+          num_of_bedroom: 1,
+          num_of_bed: 1,
+          num_of_bathroom: 1,
+          num_of_kitchen: 1,
         },
-        "ratings": [],
-        "place_facilities_attributes": []
-      }
-    }
+        ratings: [],
+        place_facilities_attributes: [],
+      },
+    };
   },
 
   methods: {
@@ -110,7 +101,8 @@ export default {
       this.full_info.room_attributes.num_of_kitchen = form.num_of_kitchen;
     },
     updateUtility(form) {
-      this.full_info.place_facilities_attributes = form.place_facilities_attributes;
+      this.full_info.place_facilities_attributes =
+        form.place_facilities_attributes;
     },
     updateRule(form) {
       this.full_info.rule_attributes = form.rule_attributes;
@@ -118,27 +110,36 @@ export default {
     updateInformation(form) {
       this.full_info.details = form.details;
     },
+    updateImage(cover, list) {
+      this.full_info.image = cover;
+      this.full_info.overviews_attributes = list;
+    },
     updatePrice(form) {
       this.full_info.policy_attributes.cancel_policy = form.cancel_policy;
-      this.full_info.policy_attributes.max_num_of_people = form.max_num_of_people;
-      this.full_info.schedule_price_attributes.normal_day_price = form.normal_day_price;
-      this.full_info.schedule_price_attributes.weekend_price = form.weekend_price;
-      this.full_info.schedule_price_attributes.cleaning_price = form.cleaning_price;
+      this.full_info.policy_attributes.max_num_of_people =
+        form.max_num_of_people;
+      this.full_info.schedule_price_attributes.normal_day_price =
+        form.normal_day_price;
+      this.full_info.schedule_price_attributes.weekend_price =
+        form.weekend_price;
+      this.full_info.schedule_price_attributes.cleaning_price =
+        form.cleaning_price;
       console.log(this.full_info);
       axios.post(`http://localhost:3000/api/v1/place/new`, this.full_info, {
         headers: {
-          authorization: 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxfQ.GKRbbyx8OqCqn2qKXo924USG5t0WU-VnsB1RXWwhMog',
-        }
-      })
+          authorization:
+            "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxfQ.GKRbbyx8OqCqn2qKXo924USG5t0WU-VnsB1RXWwhMog",
+        },
+      });
     },
-  }
-}
+  },
+};
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
-@import url('https://fonts.googleapis.com/css?family=Lato&display=swap');
-@import url('https://fonts.googleapis.com/css?family=Noto+Sans+Display&display=swap');
+@import url("https://fonts.googleapis.com/css?family=Roboto&display=swap");
+@import url("https://fonts.googleapis.com/css?family=Lato&display=swap");
+@import url("https://fonts.googleapis.com/css?family=Noto+Sans+Display&display=swap");
 
 #app {
   font-family: Noto Sans Display, sans-serif;
--- src/components/PlacePicture.vue
+++ src/components/PlacePicture.vue
@@ -7,35 +7,54 @@
         <el-step title="Giá và quy định nhận chỗ"></el-step>
       </el-steps>
     </div>
-    <br>
+    <br />
     <el-card class="box-card" shadow="hover">
-      <p style="text-align:left; font-size: larger; font-weight: bold">Hình ảnh chỗ nghỉ</p>
-      <p style="text-align:left">Một vài tấm ảnh đẹp sẽ giúp khách hàng có nhiều thiện cảm hơn về chỗ nghỉ của bạn.</p>
+      <p style="text-align: left; font-size: larger; font-weight: bold">
+        Hình ảnh chỗ nghỉ
+      </p>
+      <p style="text-align: left">
+        Một vài tấm ảnh đẹp sẽ giúp khách hàng có nhiều thiện cảm hơn về chỗ
+        nghỉ của bạn.
+      </p>
+      <!-- Upload Cover Image -->
       <el-divider></el-divider>
-      <el-upload
-          class="upload-demo"
-          action="https://jsonplaceholder.typicode.com/posts/"
-          :on-preview="handlePreview"
-          :on-remove="handleRemove"
-          :file-list="cover"
-          list-type="picture"
-          align="left"
+      <p style="text-align: left">Ảnh bìa:</p>
+      <input type="file" accept="image/*" @change="updateCoverImg" />
+      <!-- Upload Facility Image -->
+      <el-divider></el-divider>
+      <p style="text-align: left">Ảnh chỗ nghỉ:</p>
+      <input
+        type="file"
+        multiple="multiple"
+        accept="image/jpeg, image/png, image/jpg"
+        @change="updateFacilityImg"
+      />
+      <!-- <el-upload
+        class="upload-demo"
+        name="image"
+        multiple="false"
+        action="https://api.imgur.com/3/upload"
+        headers="{
+          'Authorization': 'Client-ID 4175aa8362c1b1e',
+        }"
+        v-model:file-list="this.fileList"
+        list-type="picture"
+        align="left"
       >
-        <p style="text-align:left">Ảnh bìa:</p>
+        <p style="text-align: left">Ảnh bìa:</p>
         <el-button size="medium" type="primary">Click to upload</el-button>
-      </el-upload>
-      <el-upload
-          class="upload-demo"
-          action="https://jsonplaceholder.typicode.com/posts/"
-          :on-preview="handlePreview"
-          :on-remove="handleRemove"
-          :file-list="fileList"
-          list-type="picture"
-          align="left"
+      </el-upload> -->
+      <!-- <el-upload
+        class="upload-demo"
+        name="image"
+        action="https://api.imgbb.com/1/upload?key=85566a684c63bc4fe4bba7c6b2ae856a"
+        v-model:file-list="this.fileList"
+        list-type="picture"
+        align="left"
       >
-        <p style="text-align:left">Ảnh chỗ nghỉ:</p>
+        <p style="text-align: left">Ảnh chỗ nghỉ:</p>
         <el-button size="medium" type="primary">Click to upload</el-button>
-      </el-upload>
+      </el-upload> -->
       <div align="left">
         <div class="el-upload__tip">
           File ảnh định dạng jpg/png kích thước nhỏ hơn 500kb
@@ -51,45 +70,51 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "PlacePicture",
   data() {
     return {
-      cover: [
-        {
-          name: 'cover.jpeg',
-          url: 'https://cdn.luxstay.com/rooms/13737/large/room_13737_31_1566960034.jpg',
-        },
-      ],
-      fileList: [
-        {
-          name: 'pic1.jpeg',
-          url: 'https://cdn.luxstay.com/rooms/16657/large/1542352179_screenshot_4png',
-        },
-        {
-          name: 'pic2.jpeg',
-          url: 'https://cdn.luxstay.com/rooms/17679/large/room_17679_23_1543814614.jpg',
-        },
-      ],
-    }
+      coverImg: "",
+      facilityImg: [],
+    };
   },
+
   methods: {
-    handleRemove(file, fileList) {
-      console.log(file, fileList)
+    async uploadImg(e) {
+      const hostURL =
+        "https://api.imgbb.com/1/upload?key=85566a684c63bc4fe4bba7c6b2ae856a";
+      const files = e.target.files;
+      const imgURL = [];
+      for (let i = 0; i < files.length; i++) {
+        let data = new FormData();
+        data.append("image", e.target.files[i]);
+        let res = await axios.post(hostURL, data);
+        imgURL.push(res.data.data.url);
+      }
+      return imgURL;
     },
-    handlePreview(file) {
-      console.log(file)
+    async updateCoverImg(e) {
+      let arr = await this.uploadImg(e);
+      this.coverImg = arr[0];
+    },
+    async updateFacilityImg(e) {
+      let arr = await this.uploadImg(e);
+      for (let i = 0; i < arr.length; i++) {
+        this.facilityImg.push({
+          image: arr[i],
+        });
+      }
     },
     onSubmit() {
-      this.$router.push('/price')
+      this.$emit("updateImage", this.coverImg, this.facilityImg);
+      this.$router.push("/price");
     },
     onBack() {
-      this.$router.back()
-    }
+      this.$router.back();
+    },
   },
-}
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
